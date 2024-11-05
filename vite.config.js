e58d0a347e6acfa254af0ea8b59533d81b7114c9
NilzakIndigo <nilzak.indigo05@gmail.com>
2024-11-05 15:11:25 +0100

    vercel deplyment

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/doctory-qcm-web/",
   server: {
     watch: {
       usePolling: true,
     },
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        fallback: './404.html', // This line creates a 404.html
-      },
-    },
-  },
+ 
 });
